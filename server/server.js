9215e70830915249f8806df177b9f69245353dbe
Villette Comfort <villette@live.com>
2022-10-07 16:39:57 -0700

    Added to js

--- server/server.js
+++ server/server.js
@@ -16,6 +16,10 @@ if (process.env.NODE_ENV === 'production') {
 
 app.use(routes);
 
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "build"));
+ });
+
 db.once('open', () => {
   app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
 });
