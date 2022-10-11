13b6b2862035580c5ce040acc8ed03cff3f888ca
violet978 <villette@live.com>
2022-10-11 11:31:04 -0700

    Added apollo server connection to server.js

---
+++ server/schema/resolvers.js
@@ -0,0 +1,14 @@
+const { AuthenticationError } = require('apollo-server-express');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+              return User.findOne({ _id: context.user._id });
+            }
+            throw new AuthenticationError('You need to be logged in!');
+          }
+    }
+}
