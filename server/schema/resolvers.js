51dd4680e917cde130fbc8763f98a3c75b038da5
violet978 <villette@live.com>
2022-10-11 12:12:22 -0700

    Added most of resolvers

--- server/schema/resolvers.js
+++ server/schema/resolvers.js
@@ -10,5 +10,43 @@ const resolvers = {
             }
             throw new AuthenticationError('You need to be logged in!');
           }
+    },
+
+    Mutation: {
+        addUser: async (parent, { name, email, password }) => {
+          const user = await User.create({ name, email, password });
+          const token = signToken(user);
+    
+          return { token, user };
+        },
+        login: async (parent, { email, password }) => {
+          const user = await User.findOne({ email });
+    
+          if (!user) {
+            throw new AuthenticationError('No user with this email found!');
+          }
+    
+          const correctPw = await user.isCorrectPassword(password);
+    
+          if (!correctPw) {
+            throw new AuthenticationError('Incorrect password!');
+          }
+    
+          const token = signToken(user);
+          return { token, profile };
+        },
+        saveBook: async (parent, {authors, decription, title, bookId, image}) => {
+            const book = await Book.create({ authors, decription, title, bookId, image });
+        }
     }
+
 }
+
+
+
+// login(email: String!, password: String!): Auth
+// addUser(username: String!, email: String!, password: String!): Auth
+// saveBook(authors: [Author], description: String!, title: String!, bookId: String!, image: String): User
+// removeBook(bookId: String!): User
+
+module.exports = resolvers;
